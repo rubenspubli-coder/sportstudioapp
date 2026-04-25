b3a0b079e84e8a54022a187c25b6dbf44fac3526
rubenspubli-coder <rubenspubli@gmail.com>
2026-04-25 17:13:04 -0300

    Update server.js

--- server.js
+++ server.js
@@ -132,6 +132,6 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.end(data);
   });
-});
+}); 
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
